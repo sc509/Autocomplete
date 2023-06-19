6de8e06a8cf2796d4d5b342df81ab1b3ca879e20
Sv812asx <122466233+sv812asx@users.noreply.github.com>
2023-06-19 18:53:05 +0300

    first commit

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const wrapperInput = document.querySelector('.wrapper__input');
 const results = document.querySelector('.results');
 const errorElement = document.querySelector('.error');
 
+
 function createElement(tagName, className) {
     const element = document.createElement(tagName);
     element.classList.add(className);
     return element;
-}
+};
+
 
 function debounce(func, delay) {
     let timeoutId;
@@ -18,67 +20,86 @@ function debounce(func, delay) {
             func.apply(context, args);
         }, delay);
     };
-}
+};
 
-let isRequestPending = false;
+
 const rep = createElement('ul', 'wrapper__list');
 const listSelected = createElement('ul', 'wrapper__list-selected');
-
 
 results.appendChild(rep);
 results.appendChild(listSelected);
 
-async function searchRep() {
-    try {
-        isRequestPending = true;
 
+function insertDataInElement(selectedLi, li, liButton, listSelected) {
+    const newElement = document.createElement('div');
+    const nameText = document.createTextNode(`Name: ${li.textContent}`);
+    const ownerText = document.createTextNode(`Owner: ${li.dataset.owner}`);
+    const starsText = document.createTextNode(`Stars: ${li.dataset.stars}`);
+    const br1 = document.createElement('br');
+    const br2 = document.createElement('br');
+
+    newElement.appendChild(nameText);
+    newElement.appendChild(br1);
+    newElement.appendChild(ownerText);
+    newElement.appendChild(br2);
+    newElement.appendChild(starsText);
+
+    selectedLi.insertBefore(newElement, selectedLi.firstChild);
+
+    selectedLi.appendChild(liButton);
+
+    listSelected.appendChild(selectedLi);
+
+    liButton.addEventListener('click', function() {
+        listSelected.removeChild(selectedLi);
+    });
+};
+
+
+function procceningQueryResults (data, inputValue){
+    data.items.forEach(res => {
+        const li = createElement('li', 'list__element');
+        li.textContent = res.name;
+        li.dataset.owner = res.owner.login;
+        li.dataset.stars = res.stargazers_count;
+
+        li.addEventListener('click', function() {
+            rep.removeChild(li);
+            const selectedLi = createElement('li', 'list__element-selected');
+            const liButton = createElement('button', 'wrapper__list-button')
+            insertDataInElement(selectedLi, li, liButton, listSelected);
+            wrapperInput.value = '';
+            rep.textContent = '';
+        });
+        rep.appendChild(li);
+    });
+};
+
+
+async function searchRep(event) {
+    try {
         rep.textContent = '';
         errorElement.textContent = '';
-
-        const inputValue = wrapperInput.value.trim();
+        const inputValue = event.target.value.trim();
         if (!inputValue) {
             return;
-        }
-
-        const response = await fetch(`https://api.github.com/search/repositories?q=${inputValue}`);
+        };
+        const response = await fetch(`https://api.github.com/search/repositories?q=${inputValue}&per_page=5`);
         if (response.ok) {
             const data = await response.json();
-            if (inputValue !== wrapperInput.value.trim()) {
+            if (inputValue !== event.target.value.trim()) {
                 return;
             }
-            data.items.slice(0, 5).forEach(res => {
-                const li = createElement('li', 'list__element');
-                li.textContent = res.name;
-                li.dataset.owner = res.owner.login;
-                li.dataset.stars = res.stargazers_count;
-
-                li.addEventListener('click', function() {
-                    rep.removeChild(li);
-                    const selectedLi = createElement('li', 'list__element-selected');
-                    const liButton = createElement('button', 'wrapper__list-button')
-                    selectedLi.insertAdjacentHTML('afterbegin', `<div>Name: ${li.textContent}<br>Owner: ${li.dataset.owner}<br>Stars: ${li.dataset.stars}</div>`);
-                    selectedLi.appendChild(liButton);
-                    listSelected.appendChild(selectedLi);
-                    liButton.addEventListener('click',function (){
-                        listSelected.removeChild(selectedLi);
-                    });
-                    document.querySelector('.wrapper__input').value = '';
-                    rep.textContent = '';
-                });
-                rep.appendChild(li);
-            });
-
+           procceningQueryResults(data,inputValue);
         } else {
             throw new Error('Ошибка при выполнении запроса');
         }
     } catch (error) {
         console.error(error);
         errorElement.textContent = 'Произошла ошибка при выполнении запроса';
-    } finally {
-        isRequestPending = false;
-    }
-}
+    };
+};
 
 const debouncedSearchRep = debounce(searchRep, 400);
 
-wrapperInput.addEventListener('keyup', debouncedSearchRep);
+wrapperInput.addEventListener('input', debouncedSearchRep);
